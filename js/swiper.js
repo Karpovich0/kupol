85edabb13b5b099667fc00384eb0851fd2d5bac6
Olekka <noodarius0@gmail.com>
2022-10-19 15:20:57 +0300

    index page finish pixel perfect for pc and tablet

--- js/swiper.js
+++ js/swiper.js
@@ -15,10 +15,10 @@ const swiper = new Swiper(".swiper", {
 		el: ".swiper-pagination",
 		clickable: true,
 	},
-	autoplay: {
-		delay: 3000,
-		disableOnInteraction: false,
-	},
+	// autoplay: {
+	// 	delay: 3000,
+	// 	disableOnInteraction: false,
+	// },
 });
 
 HEADER.classList.add("header-black");
