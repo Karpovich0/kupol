0ae3ae7653b12750f6fa51b35c1e0d65b51524ac
Olekka <noodarius0@gmail.com>
2022-10-20 00:00:28 +0300

    finish burger

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,14 @@
+"use strict";
 const HEADER = document.querySelector(".header");
 const SWIPER_PREV = document.querySelector(".swiper-prev");
 const SWIPER_NEXT = document.querySelector(".swiper-next");
+
+const TO_TOP = document.querySelector(".to-top");
+const BURGER = document.querySelector(".burger");
+const BODY = document.querySelector("body");
+
+let isSwiperAutoPlay = true;
+let isBurderOpened = false;
 
 const swiper = new Swiper(".swiper", {
 	// arrows
@@ -24,15 +32,18 @@ const swiper = new Swiper(".swiper", {
 HEADER.classList.add("header-black");
 SWIPER_PREV.classList.add("swiper-arrow");
 SWIPER_NEXT.classList.add("swiper-arrow");
+BURGER.classList.add("burger-black");
 
 swiper.on("slideChange", function () {
 	if (scrollY == 0) {
 		if (swiper.realIndex == 0) {
 			HEADER.classList.add("header-black");
+			BURGER.classList.add("burger-black");
 			SWIPER_PREV.classList.add("swiper-arrow");
 			SWIPER_NEXT.classList.add("swiper-arrow");
 		} else {
 			HEADER.classList.remove("header-black");
+			BURGER.classList.remove("burger-black");
 			SWIPER_PREV.classList.remove("swiper-arrow");
 			SWIPER_NEXT.classList.remove("swiper-arrow");
 		}
@@ -50,5 +61,35 @@ window.addEventListener("scroll", function () {
 			HEADER.classList.remove("header-black");
 		}
 		swiper.autoplay.start();
+	}
+});
+
+TO_TOP.addEventListener("click", function () {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
+
+BURGER.addEventListener("click", function () {
+	HEADER.classList.toggle("header-burger");
+	BODY.classList.toggle("body-burger");
+
+	if (BURGER.classList.contains("burger-black")) {
+		BURGER.classList.remove("burger-black");
+	}
+
+	if (isSwiperAutoPlay) {
+		swiper.autoplay.stop();
+		isSwiperAutoPlay = false;
+	} else {
+		swiper.autoplay.start();
+		isSwiperAutoPlay = true;
+	}
+	if (!isBurderOpened) {
+		isBurderOpened = true;
+	} else {
+		if (swiper.realIndex == 0) {
+			BURGER.classList.add("burger-black");
+		}
+		isBurderOpened = false;
 	}
 });
