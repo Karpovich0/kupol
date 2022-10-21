a47e48cb5fef00fc7ee11a80fa8f8053cda9faee
Olekka <noodarius0@gmail.com>
2022-10-21 15:53:24 +0300

    finish adaptive of catalog and 126

--- js/item-swiper.js
+++ js/item-swiper.js
@@ -1,8 +1,5 @@
-let isSwiperAutoPlay = true;
-let isBurgerOpened = false;
-
 const swiper2 = new Swiper(".swiper-two", {
-	spaceBetween: 10,
+	spaceBetween: 20,
 	slidesPerView: 4,
 	freeMode: true,
 	watchSlidesProgress: true,
@@ -14,33 +11,12 @@ const swiper = new Swiper(".swiper-one", {
 		prevEl: ".swiper-prev",
 		nextEl: ".swiper-next",
 	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
 	grabCursor: true,
-	// pagination: {
-	// 	el: ".swiper-pagination",
-	// 	clickable: true,
-	// },
 	thumbs: {
 		swiper: swiper2,
 	},
 });
-
-// HEADER.classList.add("header-black");
-// SWIPER_PREV.classList.add("swiper-arrow");
-// SWIPER_NEXT.classList.add("swiper-arrow");
-// BURGER.classList.add("burger-black");
-
-// swiper.on("slideChange", function () {
-// 	if (scrollY == 0) {
-// 		if (swiper.realIndex == 0) {
-// 			HEADER.classList.add("header-black");
-// 			BURGER.classList.add("burger-black");
-// 			SWIPER_PREV.classList.add("swiper-arrow");
-// 			SWIPER_NEXT.classList.add("swiper-arrow");
-// 		} else {
-// 			HEADER.classList.remove("header-black");
-// 			BURGER.classList.remove("burger-black");
-// 			SWIPER_PREV.classList.remove("swiper-arrow");
-// 			SWIPER_NEXT.classList.remove("swiper-arrow");
-// 		}
-// 	}
-// });
