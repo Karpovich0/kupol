f7c41614b1be0de6cecbb1f6d6dd7f45fa8bd1b5
Olekka <noodarius0@gmail.com>
2022-10-31 17:25:36 +0200

    fix small bugs with swiper-three --2

--- js/openBigSlider.js
+++ js/openBigSlider.js
@@ -5,8 +5,10 @@ const BODY = document.querySelector("body");
 
 SWIPER_ONE_SLIDERS.forEach((item) =>
 	item.addEventListener("click", function () {
-		SLIDER_COVER.classList.add("visible-slider");
-		BODY.classList.add("body-burger");
+		if (window.innerWidth > 1199) {
+			SLIDER_COVER.classList.add("visible-slider");
+			BODY.classList.add("body-burger");
+		}
 	})
 );
 
